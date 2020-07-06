580a746418c00ab5924a19a4fd3554ff9acebd59
waratchayaDCPYM <waratchaya.dcpym@gmail.com>
2020-07-06 17:10:35 +0700

    remove unuse component and rename main component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
 <v-app>
     <v-content>
-      <HelloWorld/>
+      <!-- <hocco-tuna/> -->
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
